3563a8f0826ebed9972fd9c13346b7ee670abd2a
Laurene BUTTELLI <laurene.buttelli@gmail.com>
2023-07-29 18:38:25 +0200

    add conditions

--- movieHandlers.js
+++ movieHandlers.js
@@ -60,10 +60,26 @@ const deleteMovie = (req, res) => {
 }
 
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+  const sqlValues = [];
+  
+  if (req.query.color != null) {
+    sql += " where color = ?";
+    sqlValues.push(req.query.color);
+  
+    if (req.query.max_duration != null) {
+      sql += " and duration <= ?";
+      sqlValues.push(req.query.max_duration);
+    }
+  } else if (req.query.max_duration != null) {
+    sql += " where duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+
   database
-    .query("SELECT * FROM movies")
+    .query(sql, sqlValues)
     .then(([movies]) => {
-      res.status(200).json(movies);
+      res.json(movies);
     })
     .catch((err) => {
       console.error(err);
